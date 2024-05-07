9d0038be94b96f5ef3e555bc48b2f6ed03da7214
rawidaniel <rawidaniel96@gmail.com>
2024-05-07 11:12:11 +0300

    feat: create serializer and serialize the object

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'playground',
     'store',
     'store_custom',
@@ -144,3 +145,8 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
