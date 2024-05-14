878fe16edced58d3c192617b9b78ea424d47cf5b
rawidaniel <rawidaniel96@gmail.com>
2024-05-14 08:58:35 +0300

    feat: add filtering functionality

--- store/views.py
+++ store/views.py
@@ -6,25 +6,30 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import status
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .filters import ProductFilter
 
 # Create your views here.
 
 
 class ProductViewSet(ModelViewSet):
-    # queryset = Product.objects.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['collection_id']
+    filterset_class = ProductFilter
 
 
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
+    # def get_queryset(self):
+    #     queryset = Product.objects.all()
+    #     collection_id = self.request.query_params.get('collection_id')
 
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+    #     if collection_id is not None:
+    #         queryset = queryset.filter(collection_id=collection_id)
+    #     return queryset
 
     def get_serializer_context(self):
         return {'request': self.request}
